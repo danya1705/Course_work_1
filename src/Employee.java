c4495a411ee43618e95ad42c9b62ca32602b3ca8
danya1705 <danya17@gmail.com>
2022-08-11 14:51:17 +0300

    simple course work is done

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+    int id;
+    String name;
+    int department;
+    double wages;
+    static int idCounter = 0;
+
+    public Employee(String name, int department, double wages) {
+        this.id = 1 + idCounter++;
+        this.name = name;
+        if (department > 0 && department < 6) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Не корректный номер отдела (должен быть от 1 до 5)");
+        }
+        if (wages >= 0.0) {
+            this.wages = wages;
+        } else {
+            throw new IllegalArgumentException("Зарплата не может быть отрицательной");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getWages() {
+        return wages;
+    }
+
+    public void setWages(double wages) {
+        this.wages = wages;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + name + ". Отдел №" + department + ". Зарплата " + wages + " р.";
+    }
+}
